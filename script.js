d83cd879278581670075e3108fd681c44e214e33
Jeffdev <jeffersonmariano@protonamil.com>
2023-07-13 16:07:56 -0300

    icone colocado e desvendando o problema do menu

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function ScrollReveal()
 
 ScrollReveal({ reset: true });
 ScrollReveal().reveal('.container');
+
+//para dar blink no ponto final no logo
+function blink() {
+	$('.blink').fadeOut(500);
+    $('.blink').fadeIn(500);
+}
+setInterval(blink, 1000);
